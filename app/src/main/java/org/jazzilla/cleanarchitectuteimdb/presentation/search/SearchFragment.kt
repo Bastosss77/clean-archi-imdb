cef706dee85bbb762ea7f0dfa62c3d76452fa5d8
Bastien Le Bret <bastosss77@gmail.com>
2020-03-25 18:29:05 +0100

    Add start fragment

---
+++ app/src/main/java/org/jazzilla/cleanarchitectuteimdb/presentation/search/SearchFragment.kt
@@ -0,0 +1,6 @@
+package org.jazzilla.cleanarchitectuteimdb.presentation.search
+
+import androidx.fragment.app.Fragment
+import org.jazzilla.cleanarchitectuteimdb.R
+
+class SearchFragment : Fragment(R.layout.fragment_search)
